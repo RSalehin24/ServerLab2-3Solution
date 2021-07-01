4f70f94532f6e884b1c596aeb25a5a4e3c488abc
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2021-07-01 21:28:28 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+//Static Resources
+app.use(express.static('./pulic'));
+
+//View Engine
+app.set("view engine", "ejs");
+
+//Routes
+const indexRoutes = require('./routes/index.routes');
+
+
+
+
+app.use(indexRoutes);
+
+module.exports = app;
