b1041996451768884d63dd68df01884b2c4f8899
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2021-07-08 12:58:41 +0600

    fixed app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-//Static Resources
-app.use(express.static('./pulic'));
 
+//Static Resources
+app.use(express.static("public"));
 //View Engine
 app.set("view engine", "ejs");
 
+
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+
 //Routes
-const indexRoutes = require('./routes/index.routes');
-
-
-
-
+const indexRoutes = require("./routes/index.routes");
 app.use(indexRoutes);
+
 
 module.exports = app;
